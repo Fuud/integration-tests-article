f9118159e514d15b9897104ec7b47e69b9e0c63d
Fedor Bobin <fuudtorrentsru@gmail.com>
2022-07-15 23:02:13 +0300

    5) Use nanocloud library: create separated jvm for each component

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
    microservices [rev:cfbebf68](https://github.com/Fuud/integration-tests-article/commit/cfbebf68c0876dc2bfaaca8cb3074d7c6275d414)
 3) added security to client service, but worker service is affected in tests (
    only!) [rev:7c8abae7](https://github.com/Fuud/integration-tests-article/commit/7c8abae738f827b7601bc42704c3c1e657ae09fb)
-4) Added maven-dependency-plugin:build-classpath and code to read resulting files
+4) Added maven-dependency-plugin:build-classpath and code to read resulting
+   files [rev:50d2802f](https://github.com/Fuud/integration-tests-article/commit/50d2802f9f4cbf710beb65fbd87850139b3131d6)
+5) Use nanocloud library: create separated jvm for each component
--- integration-tests/src/test/java/fuud/test/TaskIntegrationTest.java
+++ integration-tests/src/test/java/fuud/test/TaskIntegrationTest.java
@@ -2,6 +2,11 @@ package fuud.test;
 
 import fuud.client.service.ClientServiceApplication;
 import fuud.worker.service.WorkerServiceApplication;
+import org.gridkit.nanocloud.Cloud;
+import org.gridkit.nanocloud.CloudFactory;
+import org.gridkit.nanocloud.VX;
+import org.gridkit.vicluster.ViNode;
+import org.gridkit.vicluster.ViProps;
 import org.junit.Test;
 import org.springframework.http.HttpHeaders;
 
@@ -17,11 +22,32 @@ import static org.junit.Assert.assertFalse;
 public class TaskIntegrationTest {
     @Test
     public void testTaskSubmission() throws Exception {
-        System.out.println("Correct classpath for client app: " + ClassPathHelper.getClasspathForArtifact("client-service"));
-        System.out.println("Correct classpath for worker app: " + ClassPathHelper.getClasspathForArtifact("worker-service"));
 
-        ClientServiceApplication.main(new String[0]);
-        WorkerServiceApplication.main(new String[0]);
+        Cloud cloud = CloudFactory.createCloud();
+        ViNode clientNode = cloud.node("client");
+        clientNode.x(VX.CLASSPATH).inheritClasspath(false);
+        ViProps.at(clientNode).setLocalType();
+        ClassPathHelper.getClasspathForArtifact("client-service")
+                .forEach(classPathElement -> clientNode.x(VX.CLASSPATH).add(classPathElement));
+        clientNode.exec(new Runnable() {
+            @Override
+            public void run() {
+                ClientServiceApplication.main(new String[0]);
+            }
+        });
+
+        ViNode workerNode = cloud.node("worker");
+        workerNode.x(VX.CLASSPATH).inheritClasspath(false);
+        ViProps.at(workerNode).setLocalType();
+        ClassPathHelper.getClasspathForArtifact("worker-service")
+                .forEach(classPathElement -> workerNode.x(VX.CLASSPATH).add(classPathElement));
+        workerNode.exec(new Runnable() {
+            @Override
+            public void run() {
+                WorkerServiceApplication.main(new String[0]);
+            }
+        });
+
 
         HttpResponse<String> response = HttpClient.newBuilder().build().send(
                 HttpRequest.newBuilder()
